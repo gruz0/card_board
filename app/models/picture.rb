ed5b7ec32bc8d836820245d37b536e882f71b5b3
Pavel Kosykh <pavel.kosykh@rcntec.com>
2016-07-22 15:21:34 +0300

    add attach to models

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ApplicationRecord
   has_one :post
 
-
+  has_attached_file :image
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_many :pictures
   has_one :video
+
+  has_attached_file :preview
 end
