7092829f8932fabea920376aa917765d9a30c225
Pavel Kosykh <pavel.kosykh@rcntec.com>
2016-07-28 00:54:22 +0300

    create flex grid with preview images on main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,69 @@
 //= require bootstrap-sprockets
 //= require cocoon
 //= require_tree .
+
+
+
+$(document).ready(function(){
+    resizableGrid();
+
+    $(window).resize(function () {
+        resizableGrid();
+    });
+});
+
+// resizable drid 2x3
+function resizableGrid(){
+    if($(window).width() > 991) {
+        $(".col-md-4").each(function (index, value) {
+            var blockSelector = $(value);
+            var imageSelector = blockSelector.children();
+
+            if (blockSelector.width() >= blockSelector.height()) {
+                if (imageSelector.width() <= blockSelector.width()) {
+                    verticalAlign(imageSelector, blockSelector);
+                }
+
+                if (imageSelector.height() <= blockSelector.height()){
+                    horizontalAlign(imageSelector, blockSelector);
+                }
+            } else {
+                if (imageSelector.width() <= blockSelector.width()) {
+                    verticalAlign(imageSelector, blockSelector);
+                }
+
+                if (imageSelector.height() <= blockSelector.height()){
+                    horizontalAlign(imageSelector, blockSelector);
+                }
+            }
+        });
+    } else {
+        $(".col-md-4").each(function (index, value) {
+            var blockSelector = $(value);
+            var imageSelector = blockSelector.children();
+
+            imageSelector.css('max-height', 'inherit');
+            imageSelector.css('max-width', '100%');
+            imageSelector.css('position', 'inherit');
+            imageSelector.css('left', '0px');
+            imageSelector.css('top', '0px');
+        });
+    }
+
+}
+
+function verticalAlign(imageSelector, blockSelector) {
+    imageSelector.css('max-height', 'inherit');
+    imageSelector.css('max-width', '100%');
+    imageSelector.css('position', 'absolute');
+    imageSelector.css('left', 0);
+    imageSelector.css('top', -((imageSelector.width() - blockSelector.width()) / 2));
+}
+
+function horizontalAlign(imageSelector, blockSelector) {
+    imageSelector.css('max-width', 'inherit');
+    imageSelector.css('max-height', '100%');
+    imageSelector.css('position', 'absolute');
+    imageSelector.css('top', 0);
+    imageSelector.css('left', -((imageSelector.width() - blockSelector.width()) / 2));
+}
