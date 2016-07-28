25dbf1428a805bb2cb98eca8686d1048adc70d3c
Pavel Kosykh <pavel.kosykh@rcntec.com>
2016-07-28 22:13:25 +0300

    complete frontend for admin pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,25 +17,3 @@
 //= require bootstrap-sprockets
 //= require cocoon
 //= require_tree .
-
-!function () {
-    'use strict';
-
-    $(document).ready(function(){
-        $('.carousel').slick({dots: true})
-    })
-
-    $(window).on('resize', function () {
-        $('.card').height( ($(window).width()) / 4.4 );
-    }).resize();
-
-    $(document).on('click', '.card__i', function () {
-        $('.js-cards').hide()
-        $($(this).find('.card').data('target')).show()
-    })
-
-    $(document).on('click', '.content__close', function () {
-        $('.js-cards').show()
-        $('.content').hide()
-    })
-}();
